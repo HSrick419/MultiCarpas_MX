3a5a7f4b9c07bebc775e092176414e46628f6c08
Ricardo Fabian Escamilla de los Santos <richie20021102@gmail.com>
2024-06-25 18:03:34 -0600

    Actualizacion de mejora de la visualizacion, version preeliminar

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-document.getElementById("miBoton").addEventListener("click", function() {
-    alert("¡Hola! Has hecho clic en el botón.");
+// script.js
+
+$(document).ready(function() {
+    // Smooth scrolling para los enlaces de ancla
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
 });
